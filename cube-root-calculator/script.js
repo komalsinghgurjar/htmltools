c2f3133273b1368da30ce923ec70bed7f690d25c
komalsin9h <106956840+komalsin9h@users.noreply.github.com>
2023-04-15 19:24:20 +0530

    Update script.js

--- cube-root-calculator/script.js
+++ cube-root-calculator/script.js
@@ -5,5 +5,5 @@ form.addEventListener('submit', function(e) {
   e.preventDefault();
   const number = Number(document.querySelector('#number').value);
   const cubeRoot = Math.cbrt(number);
-  result.textContent = `ANSWER: <span style="color: green;"> ${cubeRoot}</span>`;
+  result.textContent = `${cubeRoot}`;
 });
