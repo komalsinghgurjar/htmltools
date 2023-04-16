06ceade21e32e10df69ee90638441c3b69771e6e
komalsin9h <106956840+komalsin9h@users.noreply.github.com>
2023-04-16 23:46:07 +0530

    Update script.js

--- pdf-reader/script.js
+++ pdf-reader/script.js
@@ -2,6 +2,10 @@ let pdfFileInput = document.getElementById('pdfFileInput');
 let readButton = document.getElementById('readButton');
 let pdfCanvas = document.getElementById('pdfCanvas');
 let pdfContainer = document.getElementById('pdfContainer');
+let prevButton = document.getElementById('prevButton');
+let nextButton = document.getElementById('nextButton');
+let pageNumInput = document.getElementById('pageNumInput');
+let totalPages = document.getElementById('totalPages');
 let pdfDoc = null;
 let currentPage = 1;
 
@@ -23,12 +27,38 @@ readButton.addEventListener('click', function() {
   }
 });
 
+prevButton.addEventListener('click', function() {
+  if (pdfDoc !== null && currentPage > 1) {
+    currentPage--;
+    renderPage(pdfDoc, currentPage);
+  }
+});
+
+nextButton.addEventListener('click', function() {
+  if (pdfDoc !== null && currentPage < pdfDoc.numPages) {
+    currentPage++;
+    renderPage(pdfDoc, currentPage);
+  }
+});
+
+pageNumInput.addEventListener('input', function() {
+  let pageNum = parseInt(this.value);
+  if (pdfDoc !== null && pageNum > 0 && pageNum <= pdfDoc.numPages) {
+    currentPage = pageNum;
+    renderPage(pdfDoc, currentPage);
+  }
+});
+
 function renderPDF(data) {
   pdfContainer.style.display = 'block';
   
   pdfjsLib.getDocument({data: data}).promise.then(function(doc) {
     pdfDoc = doc;
     readButton.disabled = false;
+    prevButton.disabled = false;
+    nextButton.disabled = false;
+    pageNumInput.disabled = false;
+    totalPages.textContent = '/ ' + pdfDoc.numPages;
     currentPage = 1;
     renderPage(pdfDoc, currentPage);
   });
@@ -37,16 +67,8 @@ function renderPDF(data) {
 function renderPage(pdfDoc, pageNum) {
   pdfDoc.getPage(pageNum).then(function(page) {
     let viewport = page.getViewport({scale: 1});
-    let canvasContext = pdfCanvas.getContext('2d');
-    
+    let scale = pdfCanvas.offsetWidth / viewport.width;
+    viewport = page.getViewport({scale: scale});
+    let context = pdfCanvas.getContext('2d');
     pdfCanvas.height = viewport.height;
     pdfCanvas.width = viewport.width;
-    
-    let renderContext = {
-      canvasContext: canvasContext,
-      viewport: viewport
-    };
-    
-    page.render(renderContext);
-  });
-}
