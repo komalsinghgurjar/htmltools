6e0cbbfc61ef57d6efa674a4e88736dc94ad93cd
komalsin9h <106956840+komalsin9h@users.noreply.github.com>
2023-04-16 23:38:51 +0530

    Update script.js

--- pdf-reader/script.js
+++ pdf-reader/script.js
@@ -1,6 +1,9 @@
 let pdfFileInput = document.getElementById('pdfFileInput');
+let readButton = document.getElementById('readButton');
 let pdfCanvas = document.getElementById('pdfCanvas');
 let pdfContainer = document.getElementById('pdfContainer');
+let pdfDoc = null;
+let currentPage = 1;
 
 pdfFileInput.addEventListener('change', function() {
   let file = this.files[0];
@@ -14,25 +17,36 @@ pdfFileInput.addEventListener('change', function() {
   reader.readAsArrayBuffer(file);
 });
 
+readButton.addEventListener('click', function() {
+  if (pdfDoc !== null && currentPage > 0 && currentPage <= pdfDoc.numPages) {
+    renderPage(pdfDoc, currentPage);
+  }
+});
+
 function renderPDF(data) {
   pdfContainer.style.display = 'block';
   
-  pdfjsLib.getDocument({data: data}).promise.then(function(pdfDoc) {
-    let numPages = pdfDoc.numPages;
+  pdfjsLib.getDocument({data: data}).promise.then(function(doc) {
+    pdfDoc = doc;
+    readButton.disabled = false;
+    currentPage = 1;
+    renderPage(pdfDoc, currentPage);
+  });
+}
+
+function renderPage(pdfDoc, pageNum) {
+  pdfDoc.getPage(pageNum).then(function(page) {
+    let viewport = page.getViewport({scale: 1});
+    let canvasContext = pdfCanvas.getContext('2d');
     
-    pdfDoc.getPage(1).then(function(page) {
-      let viewport = page.getViewport({scale: 1});
-      let canvasContext = pdfCanvas.getContext('2d');
-      
-      pdfCanvas.height = viewport.height;
-      pdfCanvas.width = viewport.width;
-      
-      let renderContext = {
-        canvasContext: canvasContext,
-        viewport: viewport
-      };
-      
-      page.render(renderContext);
-    });
+    pdfCanvas.height = viewport.height;
+    pdfCanvas.width = viewport.width;
+    
+    let renderContext = {
+      canvasContext: canvasContext,
+      viewport: viewport
+    };
+    
+    page.render(renderContext);
   });
 }
