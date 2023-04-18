331efe235b5ad5987a801f89ee08e3cee48c5511
komalsin9h <106956840+komalsin9h@users.noreply.github.com>
2023-04-18 07:52:38 +0530

    Update script.js

--- text-editor/script.js
+++ text-editor/script.js
@@ -1,25 +1,35 @@
 function saveFile() {
   var text = document.getElementById("editor").value;
-  var encoding = "utf-8"; // default encoding
-  var extension = ".txt"; // default extension
 
-  // get selected encoding
-  var encodingSelect = document.getElementById("encoding");
-  if (encodingSelect) {
-    encoding = encodingSelect.value;
-  }
-
-  // get selected extension
-  var extensionSelect = document.getElementById("extension");
-  if (extensionSelect) {
-    extension = extensionSelect.value;
+  // prompt user for encoding
+  var encoding = prompt("Please select an encoding:", "UTF-8");
+  if (!encoding) {
+    encoding = "UTF-8"; // default encoding
   }
 
   // create blob and download link
-  var blob = new Blob([text], {type: "text/plain;charset=" + encoding});
+  var blob;
+  if (encoding.toUpperCase() === "UTF-8") {
+    blob = new Blob([text], {type: "text/plain;charset=utf-8"});
+  } else {
+    blob = new Blob([new TextEncoder(encoding).encode(text)], {type: "text/plain;charset=" + encoding});
+  }
+  
+  // prompt user for file name and extension
+  var fileName = prompt("Please enter a file name:", "file");
+  if (!fileName) {
+    fileName = "file"; // default file name
+  }
+  var extension = prompt("Please enter a file extension (e.g. .txt, .html):", ".txt");
+  if (!extension) {
+    extension = ".txt"; // default extension
+  }
+  var fullName = fileName + extension;
+
+  // create download link
   var url = URL.createObjectURL(blob);
   var link = document.createElement("a");
   link.href = url;
-  link.download = "file" + extension;
+  link.download = fullName;
   link.click();
 }
