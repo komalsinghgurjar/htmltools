4d72ae4dd4853a39d8988dcd0df445c7c0facafc
komalsin9h <106956840+komalsin9h@users.noreply.github.com>
2023-05-16 07:19:40 +0530

    Update script.js

--- amazon-gift-card-sell/script.js
+++ amazon-gift-card-sell/script.js
@@ -5,6 +5,10 @@ var submitData=null;
 const checkbox = document.getElementById('checkbox');
   const submitButton = document.getElementById('submitReview');
 
+var spanElement = document.getElementById("mySpan");
+  spanElement.innerHTML = percentage;
+
+
 submitButton.addEventListener('click', function() {
   
     
