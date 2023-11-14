4e4fa3d5bcd8a591476d8a40de4c7627bcfa7462
Komal Gurjar <106956840+komalsinghgurjar@users.noreply.github.com>
2023-11-14 11:18:39 +0530

    Create analytics.js

---
+++ analytics.js
@@ -0,0 +1,13 @@
+// analytics.js
+
+// Include Google Tag Manager script
+var gtagScript = document.createElement('script');
+gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-FBL87273FY';
+gtagScript.async = true;
+document.head.appendChild(gtagScript);
+
+// Your Google Analytics code
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-FBL87273FY');
