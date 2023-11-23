d2f38bbc7fb3496aae1bab081f9ce172b6cecd70
Komal Singh <komalsin9h@gmail.com>
2023-11-23 12:58:35 +0530

    webtools integration

--- cube-root-calculator/script.js
+++
@@ -1,9 +0,0 @@
-const form = document.querySelector('form');
-const result = document.querySelector('#result');
-
-form.addEventListener('submit', function(e) {
-  e.preventDefault();
-  const number = Number(document.querySelector('#number').value);
-  const cubeRoot = Math.cbrt(number);
-  result.textContent = `${cubeRoot}`;
-});
